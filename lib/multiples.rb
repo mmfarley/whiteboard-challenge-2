ee743ee5dcfbdb8a7dedc1b29415f223604c1b38
Megan Farley <megfarley4@gmail.com>
2018-10-23 10:54:47 -0500

    whiteboard finished meg

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,18 @@
 # Enter your procedural solution here!
+
+def collect_multiples(limit)
+    multiples = []
+    count = limit - 1
+    while count > 0
+        if count % 3 == 0 || count % 5 == 0
+            multiples << count
+        end
+        count -= 1
+    end
+    multiples.reverse
+end
+
+def sum_multiples(limit)
+    multiples = collect_multiples(limit)
+    multiples.inject(0, :+)
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,27 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+    attr_accessor :limit
+    
+    def initialize(limit)
+        self.limit = limit
+    end
+
+    def collect_multiples
+        multiples = []
+        count = self.limit - 1
+        while count > 0
+           if count % 3 == 0 || count % 5 == 0
+               multiples << count
+           end
+           count -= 1
+        end
+     multiples.reverse
+    end
+
+    def sum_multiples
+        multiples = collect_multiples
+        multiples.inject(0, :+)
+    end
+
+end
